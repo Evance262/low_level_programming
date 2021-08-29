7566affa8523a38b9def520eb1342f3340fe6bec
Evans262 <evancechapuma62@gmail.com>
2021-08-29 22:41:25 +0200

    + and - program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,14 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n > 0)
 {
 printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n"), n;
+printf("%d is zero\n", n);
 }
 else
 {
