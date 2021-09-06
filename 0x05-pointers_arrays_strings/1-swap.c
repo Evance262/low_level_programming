f39be8f57852dac35ab1be09a930999f59ed8179
Evans262 <evancechapuma62@gmail.com>
2021-09-06 21:56:39 +0200

    swap factor

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
  * swap_int - swaps the values of two integers
- * a and b to *a and *b
- * @n: pointer to an int
+ *
+ * @a: pointer to an int
+ * @b: pointer to other int
  * Return: Nothing
  */
 void swap_int(int *a, int *b)
 {
-    *a = 42;
-    *b = 98;
+int aux;
+*a = *b;
+*b = aux;
 }
