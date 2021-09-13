ee12060ce891f6e348fd9f96d162826c697c7858
Evans262 <evancechapuma62@gmail.com>
2021-09-13 14:53:42 +0200

    locating characters in a program

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 #include <string.h>
 /**
- * 
+ * _strchr - function that locates a character in a string.
+ * @s: where s is a string
+ * @c: character on the first occurence
+ * Returns: a pointer
 */
 char *_strchr(char *s, char c)
 {
-
-
-    return (0);
+int i = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+return (0);
 }
