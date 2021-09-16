8bf1ff27ca512fcae7cf8915fbe3a579ac8e8064
Evans262 <evancechapuma62@gmail.com>
2021-09-17 01:30:13 +0200

    addition of int

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n, i, num, sum = 0;
+i = 0;
+while (i < n)
+{
+if (num >= 0)
+{
+sum = sum + num;
+}
+else
+{
+printf("Error");
+}
+i++;
+}
+printf("%d", sum);
+return (0);
+}
