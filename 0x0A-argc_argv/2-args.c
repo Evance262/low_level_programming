92cae52ffc50e35bbdf7a6491477048ca92eca3d
Evans262 <evancechapuma62@gmail.com>
2021-09-17 00:45:46 +0200

    number of arguments recieved

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * main - check the code
+ * @argc: count of the arguments supplied to the program
+ * @argv: an array of pointers to the
+ * strings which are those arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+        while(argc--)
+                printf("%s\n", *argv++);
+        exit(EXIT_SUCCESS);
+}
