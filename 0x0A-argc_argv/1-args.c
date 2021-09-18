351c1c1cca454eb7cb7cc8c437b11a9099e21dff
Evans262 <evancechapuma62@gmail.com>
2021-09-18 08:31:40 +0200

    argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,10 +10,6 @@
  */
 int main(int argc, char *argv[])
 {
-int counter;
-for (counter = 0; counter < argc; counter++)
-printf("%2d\n", counter, argv[counter]);
-printf("\n");
-
+printf("%d\n", argc);
 return (0);
 }
