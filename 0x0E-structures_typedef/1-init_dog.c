89fc18c6bfe40b539165baf11d093e8dc549d876
Evans262 <evancechapuma62@gmail.com>
2021-09-30 01:01:36 +0200

    printing a name with pointers

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * 
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog d1;
+d1.name = "popp";
+d1.age = 3.5;
+d1.owner = "Mike";
+return (0);
+}
