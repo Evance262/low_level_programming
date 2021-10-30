e4d40d25448f357581b355dbbf5ba8f380f73953
Evans262 <evancechapuma62@gmail.com>
2021-10-31 00:12:46 +0200

    takes first two integer arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+
 /**
- * main - check the code
- * @argc: count of the arguments supplied to the program
- * @argv: an array of pointers to the
- * strings which are those arguments
- *
- * Return: Always 0.
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
  */
+
 int main(int argc, char *argv[])
-{int i, mul =0;
-if (argc > 1)
 {
-for (i = 1; i < argc; i++)
+int i, j;
+
+if (argc == 1 || argc == 2)
 {
-mul = mul * atoi(argv[i]);
-}
-printf("%d\n", mul);
+printf("Error\n");
+return (1);
 }
 else
 {
-    printf("Error");
+j = 1;
+
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+
+printf("%d\n", j);
 }
 
 return (0);
