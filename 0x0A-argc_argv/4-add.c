4220d8278033672a3ac1d406cd0b6b99c9a029fc
Evans262 <evancechapuma62@gmail.com>
2021-10-31 00:18:48 +0200

    fixed some bugs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "holberton.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
+
 int main(int argc, char *argv[])
-{int i, mul =0;
-if (argc > 1)
 {
-for (i = 1; i < argc; i++)
-{
-mul += atoi(argv[i]);
-}
-printf("%d\n", mul);
-}
+int i, j, length, sum;
+char *ptr;
+
+if (argc < 2)
+printf("0\n");
 else
 {
-    printf("Error\n");
-    return (1);
+sum = 0;
+for (i = 1; i < argc; i++)
+{
+ptr = argv[i];
+length = strlen(ptr);
+
+for (j = 0; j < length; j++)
+{
+if (isdigit(*(ptr + j)) == 0)
+{
+printf("Error\n");
+return (1);
+}
 }
 
+sum += atoi(argv[i]);
+}
+
+printf("%d\n", sum);
+}
 return (0);
 }
