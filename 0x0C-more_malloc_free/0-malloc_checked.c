4407b71c1766c9ee8f6e171db10585a39bbc2856
Evans262 <evancechapuma62@gmail.com>
2021-11-01 00:49:01 +0200

    fixed some bugs

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * create_array - creates an array of chars,
- * and initializes it with a specific char
- * @size: the size of an array
- * @c: character value
- * Return: a pointer to the array, or NULL if it fails
-*/
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
 void *malloc_checked(unsigned int b)
+{
+void *mem = malloc(b);
+
+if (mem == NULL)
+exit(98);
+
+return (mem);
+}
